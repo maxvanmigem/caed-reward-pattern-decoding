bece39424c5acc5af01b6095232034a2e3067eb4
Maximilien Van Migem <maximilien.vanmigem@ugent.be>
2023-05-26 15:14:10 +0200

    Update kobayashi_mvpa.py

--- kobayashi_mvpa.py
+++ kobayashi_mvpa.py
@@ -1,4 +1,8 @@
 # %%
+"""
+Load packages
+"""
+
 import nilearn, os, glob
 
 from nilearn import plotting
@@ -14,17 +18,30 @@ import pandas as pd
 Create data paths and intialize base variables 
 """
 
-data_dir_path = 'C:/Users/Maximilien/OneDrive - UGent/Case_studies_analysis_of_exp_data/data/' #change this to your data directory
+data_dir_path = 'C:/Users/Maximilien/OneDrive - UGent/Case_studies_analysis-of_exp_data/data/ds003758/derivatives/' #change this to your data directory
 
-# Make a nested list to refer to different participant paths to func files indexed like this [subject,run]
+# Make a nested lists to refer to different participant paths indexed like this [subject][run]
 sub_dir_path = glob.glob(data_dir_path + 'sub-*')
-paths = []
-
-for ind,name in enumerate(sub_dir_path):        # for the func files
-    func_fnames = glob.glob(name + '/func/*.nii.gz')
-    paths.append(func_fnames)
+anat_paths = []
+fmap_paths = []
+func_paths = []
 
 
+for ind,name in enumerate(sub_dir_path):
+
+    anat_fnames = glob.glob(name + '/anat/*.nii.gz')# for the anat files
+    anat_paths.append(anat_fnames)
+
+    fmap_fnames = glob.glob(name + '/fmap/*.nii.gz')# for the fmap files
+    fmap_paths.append(fmap_fnames)
+
+    func_fnames = glob.glob(name + '/func/*.nii.gz')# for the func files
+    func_paths.append(func_fnames)
+
+
+# the number of subjects can be derrived from the length of sub_dir_path list and the number
+n_sub = len(sub_dir_path)
+n_runs = len(anat_paths[0])
 
 
 # %%
@@ -32,10 +49,26 @@ for ind,name in enumerate(sub_dir_path):        # for the func files
 
 """
 
+for i,e in enumerate(anat_paths[0]):
+    print(e)
 
 
 
 # %%
-plotting.view_img(image.mean_img(fname), threshold=None)
+plotting.view_img(image.mean_img(func_paths[0][0]), threshold=None)
+
+# %%
+s1_r1 = nilearn.image.load_img(func_paths[0][0])
+
+# %%
+test_slice1 = image.index_img(func_paths[1][0],'right')
+test_slice2 = image.index_img(func_paths[1][0],3)
+plotting.view_img(test_slice1)
+
+# %%
+print(s1_r1.header)
+
+# %%
+
 
 
