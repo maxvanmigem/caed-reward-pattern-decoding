c7e4005b9bc02395de703dc8c0c84be9da83bba8
Maximilien Van Migem <maximilien.vanmigem@ugent.be>
2023-06-11 18:34:01 +0200

    Update roi_gen.py
    made data paths relative

--- roi_gen.py
+++ roi_gen.py
@@ -1,6 +1,6 @@
 # %%
 """ 
-Script to extract, compute and plot roi images 
+Script to extract, compute and plot roi images
 Using Julich-Brain Atlas: https://search.kg.ebrains.eu/instances/99437b56-3add-4d19-9bfd-5b413dbf5173
 """
 # Written by Merel De Merlier, Sam Vandermeulen and Max Van Migem
@@ -22,10 +22,10 @@ Load and plot atlas images
 """
 # Load atlas image of ventral striatum and medial orbitofrontal cortex 
 # These atlas images are probabilistic so we binarize them first to get roi's with set boundaries
-lvs_roi = image.binarize_img('H:/Documents/cases_in_analysis_of_exp_data/roi/FUP_FUNDUS_OF_PUTAMEN_VENTRAL_STRIATUM_LEFT.nii.gz', threshold=0.1)
-rvs_roi = image.binarize_img('H:/Documents/cases_in_analysis_of_exp_data/roi/FUP_FUNDUS_OF_PUTAMEN_VENTRAL_STRIATUM_RIGHT.nii.gz', threshold=0.1)
-lmofc_roi = image.binarize_img('H:/Documents/cases_in_analysis_of_exp_data/roi/AREA_FO3_OFC_LEFT.nii.gz', threshold=0.1)
-rmofc_roi = image.binarize_img('H:/Documents/cases_in_analysis_of_exp_data/roi/AREA_FO3_OFC_RIGHT.nii.gz', threshold=0.1)
+lvs_roi = image.binarize_img('./roi/FUP_FUNDUS_OF_PUTAMEN_VENTRAL_STRIATUM_LEFT.nii.gz', threshold=0.1)
+rvs_roi = image.binarize_img('./roi/FUP_FUNDUS_OF_PUTAMEN_VENTRAL_STRIATUM_RIGHT.nii.gz', threshold=0.1)
+lmofc_roi = image.binarize_img('./roi/AREA_FO3_OFC_LEFT.nii.gz', threshold=0.1)
+rmofc_roi = image.binarize_img('./roi/AREA_FO3_OFC_RIGHT.nii.gz', threshold=0.1)
 
 # Different combination to see if there are some that are more effective at predicting reward magnitude
 bi_vs_roi = image.math_img('mask1 + mask2', mask1 = lvs_roi, mask2 = rvs_roi)
@@ -50,14 +50,14 @@ plotting.plot_roi(all_roi,title='Complete system',black_bg=True)
 Save images
 """
 
-nib.save(lvs_roi, 'H:/Documents/cases_in_analysis_of_exp_data/roi/clean_roi/lvs_roi.nii.gz')
-nib.save(rvs_roi, 'H:/Documents/cases_in_analysis_of_exp_data/roi/clean_roi/rvs_roi.nii.gz')
-nib.save(lmofc_roi, 'H:/Documents/cases_in_analysis_of_exp_data/roi/clean_roi/lmofc_roi.nii.gz')
-nib.save(rmofc_roi, 'H:/Documents/cases_in_analysis_of_exp_data/roi/clean_roi/rmofc_roi.nii.gz')
-nib.save(bi_vs_roi, 'H:/Documents/cases_in_analysis_of_exp_data/roi/clean_roi/bi_vs_roi.nii.gz')
-nib.save(bi_mofc_roi, 'H:/Documents/cases_in_analysis_of_exp_data/roi/clean_roi/bi_mofc_roi.nii.gz')
-nib.save(left_roi, 'H:/Documents/cases_in_analysis_of_exp_data/roi/clean_roi/left_roi.nii.gz')
-nib.save(right_roi, 'H:/Documents/cases_in_analysis_of_exp_data/roi/clean_roi/right_roi.nii.gz')
-nib.save(all_roi, 'H:/Documents/cases_in_analysis_of_exp_data/roi/clean_roi/all_roi.nii.gz')
+nib.save(lvs_roi, './roi/clean_roi/lvs_roi.nii.gz')
+nib.save(rvs_roi, './roi/clean_roi/rvs_roi.nii.gz')
+nib.save(lmofc_roi, './roi/clean_roi/lmofc_roi.nii.gz')
+nib.save(rmofc_roi, './roi/clean_roi/rmofc_roi.nii.gz')
+nib.save(bi_vs_roi, './roi/clean_roi/bi_vs_roi.nii.gz')
+nib.save(bi_mofc_roi, './roi/clean_roi/bi_mofc_roi.nii.gz')
+nib.save(left_roi, './roi/clean_roi/left_roi.nii.gz')
+nib.save(right_roi, './roi/clean_roi/right_roi.nii.gz')
+nib.save(all_roi, './roi/clean_roi/all_roi.nii.gz')
 
 
